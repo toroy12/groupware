ba367a819dd9bf77dcf5f92d4fc72e97fc337118
toroy12 <toroy12@naver.com>
2024-01-23 20:02:04 +0900

    그룹웨어 제작
    -개발환경설정
    -DB연동

---
+++ groupware/src/main/java/com/groupware/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.groupware.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String main(Model model) {
+		
+		return "index";
+	}
+}
